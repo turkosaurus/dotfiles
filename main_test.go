dc764ad1062ff2eb32e874ee06ff9528904ac0f7
turk <turk@ddbg.io>
2024-08-18 17:39:10 -0600

    tests pass

--- main_test.go
+++ main_test.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestMain(t *testing.T) {
-	word := "hello world"
-	cmd := exec.Command("echo", word)
-	output, err := cmd.Output()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%s", output)
-}
-
 func TestSpellcheck(t *testing.T) {
-	cmd := exec.Command("sh", "-c", "bin/tests/shellcheck")
+	cmd := exec.Command("sh", "-c", "tests/shellcheck")
 	t.Logf("command: %s\n", cmd.String())
 	output, err := cmd.Output()
 	t.Logf("output:\n%s\n", output)
