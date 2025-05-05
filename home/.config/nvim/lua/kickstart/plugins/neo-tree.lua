60c7494ca9912985538b53c98a38c771dd80a58d
turk <turk@grackle.club>
2025-05-04 23:20:28 -0600

    lots of neovim config setup

--- home/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ home/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,37 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
     },
-  },
+    cmd = "Neotree",
+    keys = {
+        { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+            window = {
+                position = "right",
+                mappings = {
+                    ["\\"] = "close_window",
+                },
+            },
+        },
+        default_component_configs = {
+            icon = {
+                folder_closed = "[+]", -- Closed folder icon
+                folder_open = "[-]", -- Open folder icon
+                folder_empty = "[ ]", -- Empty folder icon
+                default = "-", -- Default file icon
+            },
+        },
+    },
 }
