9fd811ab388555ab3845b16613119d0151eab333
turk <turk@grackle.club>
2025-05-09 14:24:23 -0600

    questionable: comment out window navigation which maybe clobbers keybinds

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -135,31 +135,31 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
 --
-vim.keymap.set(
-    { "n", "t" },
-    "<C-l>",
-    "<C-w><C-l>",
-    { desc = "Move focus to the right window" }
-)
-vim.keymap.set(
-    { "n", "t" },
-    "<C-h>",
-    "<C-w><C-h>",
-    { desc = "Move focus to the left window" }
-)
-vim.keymap.set(
-    { "n", "t" },
-    "<C-j>",
-    "<C-w><C-j>",
-    { desc = "Move focus to the lower window" }
-)
-vim.keymap.set(
-    { "n", "t" },
-    "<C-k>",
-    "<C-w><C-k>",
-    { desc = "Move focus to the upper window" }
-)
-
+-- vim.keymap.set(
+--     { "n", "t" },
+--     "<C-l>",
+--     "<C-w><C-l>",
+--     { desc = "Move focus to the right window" }
+-- )
+-- vim.keymap.set(
+--     { "n", "t" },
+--     "<C-h>",
+--     "<C-w><C-h>",
+--     { desc = "Move focus to the left window" }
+-- )
+-- vim.keymap.set(
+--     { "n", "t" },
+--     "<C-j>",
+--     "<C-w><C-j>",
+--     { desc = "Move focus to the lower window" }
+-- )
+-- vim.keymap.set(
+--     { "n", "t" },
+--     "<C-k>",
+--     "<C-w><C-k>",
+--     { desc = "Move focus to the upper window" }
+-- )
+--
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
