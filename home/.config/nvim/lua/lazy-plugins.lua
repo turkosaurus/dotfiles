02cabbf0475d7b71ec63c715f90b8dc4022436ee
turk <turk@grackle.club>
2025-05-10 22:00:36 -0600

    add spell checker, and nonfunctional spellcheck

--- home/.config/nvim/lua/lazy-plugins.lua
+++ home/.config/nvim/lua/lazy-plugins.lua
@@ -36,6 +36,18 @@ require("lazy").setup({
             },
         },
     },
+    -- {
+    --     "jose-elias-alvarez/null-ls.nvim",
+    --     dependencies = { "nvim-lua/plenary.nvim" },
+    --     config = function()
+    --         local null_ls = require "null-ls"
+    --         null_ls.setup {
+    --             sources = {
+    --                 null_ls.builtins.diagnostics.shellcheck, -- Add shellcheck
+    --             },
+    --         }
+    --     end,
+    -- },
     -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
     --
     -- This is often very useful to both group configuration, as well as handle
