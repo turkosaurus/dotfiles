bfb93b84c055ea2b71619bc9a5a4dbbacb38283b
turk <55367187+turkosaurus@users.noreply.github.com>
2025-09-21 11:46:27 -0600

    fuckery

--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -11,7 +11,10 @@ vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- cursor
 vim.opt.cursorline = true
+
 vim.opt.scrolloff = 10
 vim.opt.confirm = true
 vim.opt.tabstop = 4
