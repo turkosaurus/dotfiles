5ee7b2c34c1731eda25ba73cce4f1a5d1f49ecbe
f23028f9 <f23028f9@example.com>
2026-01-27 18:15:52 -0700

    exit on dotsync failure

--- init.sh
+++ init.sh
@@ -5,21 +5,24 @@ set -euo pipefail
 dot_path="$HOME/dotfiles"
 repo_path="https://github.com/turkosaurus/dotfiles"
 if [[ ! -d "$dot_path" ]]; then
-    echo "dotfiles not found, cloning anew... ($dot_path)"
-	if ! command -v git &> /dev/null; then
-	    echo "git required but not found"
-	    return 1
+	echo "dotfiles not found, cloning anew... ($dot_path)"
+	if ! command -v git &>/dev/null; then
+		echo "git required but not found"
+		return 1
 	fi
-	# suppress any login requirements 
+	# suppress any login requirements
 	if ! GIT_TERMINAL_PROMPT=0 git clone "$repo_path" "$dot_path"; then
-	    echo "error: git clone failed"
-	    return 1
+		echo "error: git clone failed"
+		return 1
 	fi
 fi
 
 echo "running dotsync..."
 cd "$dot_path" || return 1
-./home/bin/dotsync -v
+if ! ./home/bin/dotsync -v; then
+	echo "error: dotsync failed"
+	return 1
+fi
 
 dot_bin_path="$dot_path/bin"
 echo "updating path: PATH=\$PATH:$dot_bin_path"
@@ -27,26 +30,25 @@ export PATH="$PATH:$dot_bin_path"
 
 # shellcheck disable=SC1090
 if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
-    echo "sourcing shell config..."
-    # Source the shell config we may have just created, modified, or linked.
-    case "$SHELL" in
+	echo "sourcing shell config..."
+	# Source the shell config we may have just created, modified, or linked.
+	case "$SHELL" in
 	*bash)
-	    echo "Sourcing bashrc..."
-	    source ~/.bashrc
-	    ;;
+		echo "Sourcing bashrc..."
+		source ~/.bashrc
+		;;
 	*zsh)
-	    echo "Sourcing zshrc..."
-	    source ~/.zshrc
-	    ;;
+		echo "Sourcing zshrc..."
+		source ~/.zshrc
+		;;
 	*)
-	    echo "No config file to source for shell: $SHELL"
-	    ;;
-    esac
+		echo "No config file to source for shell: $SHELL"
+		;;
+	esac
 else
-    echo "cannot update shell config, not sourced."
-    echo "restart your terminal to apply changes, or run:"
-    echo "  source $0"
+	echo "cannot update shell config, not sourced."
+	echo "restart your terminal to apply changes, or run:"
+	echo "  source $0"
 fi
 
 echo "dotfiles init complete."
-
