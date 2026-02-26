efa0b7b9776dcaa90b8db5a9b60b75c14d4b99b8
turk <55367187+turkosaurus@users.noreply.github.com>
2026-02-25 21:56:50 -0700

    fixes

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {},
-	config = function(_, opts)
-		require("nvim-treesitter.config").setup(vim.tbl_extend("force", {
-			ensure_installed = { "lua", "vim", "vimdoc" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			fold = { enable = true },
-		}, opts))
-	end,
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = { "lua", "vim", "vimdoc" },
+		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+		fold = { enable = true },
+	},
 }
