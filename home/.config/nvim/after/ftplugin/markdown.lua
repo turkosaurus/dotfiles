cd98c157f591f6f3e68c06d68c582d8de0d62407
turk <turk@grackle.club>
2026-02-27 22:18:35 -0700

    copilot and completion

--- home/.config/nvim/after/ftplugin/markdown.lua
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -2,3 +2,7 @@ vim.opt_local.spell = true
 vim.opt_local.spelllang = "en"
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
+
+-- Kill switch: disable LLM/predictive text
+vim.b.copilot_suggestion_auto_trigger = false
+vim.b.completion = false
