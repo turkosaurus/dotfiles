778704958e8397f3e312a73bc797225360f8d01a
turk <55367187+turkosaurus@users.noreply.github.com>
2026-02-27 20:23:53 -0700

    lsp updates

---
+++ home/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,16 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			sh = { "shellcheck" },
+			bash = { "shellcheck" },
+		}
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
