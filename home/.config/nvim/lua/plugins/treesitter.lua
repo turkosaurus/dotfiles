30a42f37622761b8e55e732ff6541c8e3c2ee449
turk <turk@grackle.club>
2026-03-03 16:08:28 -0700

    treesitter

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,10 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = { "go", "lua", "vim", "vimdoc" },
 			auto_install = true,
-			highlight = {
-				enable = true,
-			},
 		})
+		vim.api.nvim_set_option_value("syntax", "off", { scope = "global" })
 	end,
 }
