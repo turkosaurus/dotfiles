1f6ff8d9c0c71d7a7677ce4e6d689ab855d138e9
turk <turk@grackle.club>
2026-03-03 15:51:42 -0700

    fix lsp

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,12 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
+		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "go", "lua", "vim", "vimdoc" },
 			auto_install = true,
+			highlight = {
+				enable = true,
+			},
 		})
 	end,
 }
