038493abfa4d70c677e372e297d6f2cf66584fcb
turk <55367187+turkosaurus@users.noreply.github.com>
2026-04-03 17:47:28 -0600

    changes

--- home/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"christoomey/vim-tmux-navigator",
-	keys = {
-		{ "<C-h>", "<cmd>TmuxNavigateLeft<CR>",  desc = "navigate left" },
-		{ "<C-j>", "<cmd>TmuxNavigateDown<CR>",  desc = "navigate down" },
-		{ "<C-k>", "<cmd>TmuxNavigateUp<CR>",    desc = "navigate up" },
-		{ "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "navigate right" },
-	},
-}
