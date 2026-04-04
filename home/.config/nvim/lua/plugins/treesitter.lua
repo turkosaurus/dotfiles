469d24ae3351ca566d24de1051786faa27d7e2a2
turk <55367187+turkosaurus@users.noreply.github.com>
2026-04-04 16:38:16 -0600

    tressitter weirdness

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,4 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter").setup({
-			ensure_installed = { "go", "lua", "vim", "vimdoc" },
-			auto_install = true,
-		})
-		vim.api.nvim_set_option_value("syntax", "off", { scope = "global" })
-	end,
+	enabled = false,
 }
